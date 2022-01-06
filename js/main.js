08c2a25b7a5f4d6e55b5040a85e2e0879700faa0
Rafael Herrera Amedey <rherrera.alumno@iesrafaelalberti.es>
2022-01-06 22:56:48 +0100

    Añadido captcha, validaciones y expresiones regulares. Correcion de errores.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 "use strict";
+// Variables
+let captcha = [];
+const user = /^[a-zA-Z0-9]{4,16}$/
+const pass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const mail = /[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}/;
 // Funcion que se ejecuta cuando el DOM del HTML esta completamente cargado.
 document.addEventListener("DOMContentLoaded", function () {
     let btn = document.querySelector('.hamburger');
@@ -12,21 +17,27 @@ document.addEventListener("DOMContentLoaded", function () {
 // Funcion que recoge datos del contacto.
 function enviardatos(){
     let email = document.querySelector("#email").value;
+    let descripcion = document.querySelector("#descripcion").value;
+    let asunto = document.querySelector("#asunto").value;
     if (email=="") {
-        console.log("Introduce un correo.")
+        alert("Introduce un correo.")
+    }else if(mail.test(email.value)){
+        alert("Correo no válido.");
+        return;
     }else{
         console.log(email);
     }
     
-    let asunto = document.querySelector("#asunto").value;
     if (asunto=="") {
-        console.log("Introduce un asunto.")
+        alert("Introduce un asunto.");
+        return;
     }else{
         console.log(asunto);
     }
-    let descripcion = document.querySelector("#descripcion").value;
+    
     if (descripcion=="") {
-        console.log("Introduce una descripcion.")
+        alert("Introduce una descripcion.")
+        return;
     }else{
         console.log(descripcion);
     }
@@ -35,13 +46,16 @@ function enviardatos(){
 function valida_acceso(){
     let usuario = document.querySelector("#usuario").value;
     let passwd = document.querySelector("#passwd");
-
-    console.log()
-    if(passwd.value.length>8){
-        passwd.insertAdjacentHTML("afterend","<p>Contraseña valida</p>");
+    if( !(user.test(usuario))){
+        alert("El usuario introducido no es correcto.");
+        return;
+    }else if(passwd.value.length>8 && usuario.length!=0){
+        alert("Acceso correcto.");
         console.log(usuario);
+        return;
     }else{
-        passwd.insertAdjacentHTML("afterend","<p>Contraseña no valida</p>");
+        alert("Usuario o contraseña incorrectos.");
+        return;
     }
 }
 
@@ -50,11 +64,59 @@ function valida_registro(){
     let usuario = document.querySelector("#usuario").value;
     let passwd = document.querySelector("#passwd");
     let repasswd = document.querySelector("#repasswd");
-
-    console.log(usuario);
-    if(passwd.value == repasswd.value){
-        repasswd.insertAdjacentHTML("afterend","<p>Registro correcto.</p>");
+    if(!(user.test(usuario))){
+        alert("El usuario introducido no es válido.");
+        return;
+    }else if(!(pass.test(passwd.value))){
+        alert("La contraseña introducida no es válida.")
+        return;
+    }else if((passwd.value == repasswd.value) && usuario.length!=0){
+        alert("Registro correcto.")
+        console.log(usuario);
+        return;
+    }else if(passwd.value != repasswd.value){
+        alert("Las contraseñas no coinciden.");
+        return;
     }else{
-        repasswd.insertAdjacentHTML("afterend","<p>Las contraseñas no coinciden.</p>");
+        alert("Rellene los campos.");
+        return;
+    }
+}
+// Función para crear captcha.
+function crear_captcha(){
+    let captchaHTML = document.querySelector('.captcha');
+    for (let i = 0; i < 8; i++) {
+        if(i%2==0){
+            captcha[i] = String.fromCharCode(Math.floor(Math.random()*26+65));
+        }else{
+            captcha[i] = Math.floor((Math.random() * 9) + 1);
+        }
+    }
+    let captchaTexto = captcha.join("");
+    captchaHTML.innerHTML = `${captchaTexto}`;
+}
+
+function valida_captcha(){
+    let input_captcha = document.querySelector("#captcha");
+    let input_captcha_formateado = input_captcha.value.toUpperCase();
+    let error = document.querySelector(".error");
+    let last_input = document.querySelector(".btn:last-child");
+    let captchaTexto = captcha.join("");
+    if (input_captcha.value.length==0){
+        error.innerHTML = "Introduce los caracteres del captcha.";
+        input_captcha.focus();
+        return;
+    }else if(input_captcha_formateado != captchaTexto){
+        error.innerHTML = "Los caracteres no coinciden.";
+        input_captcha.focus();
+        return;
+    }else{
+        error.innerHTML = "Captcha correcto.";
+        console.log("Captcha correcto.");
+        if(last_input.value=="ENVIAR"){
+            enviardatos();
+        }else{
+            valida_registro();
+        }
     }
 }
